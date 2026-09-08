e9737a91182ea5b4b4acf75f2408f6c97d929375
NattyPie <nppatole@brc.pshs.edu.ph>
2026-09-08 23:31:02 +0800

    Implement FantasyBook class with attributes and methods
    Added a FantasyBook class with attributes and methods for reading and researching.

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,1 +1,27 @@
-class Fantasy 
+class Fantasy Book:
+
+    def __init__(self, author, main_character, time_period, target_audience, ending):
+      self.author = author
+      self.main_character = main_character
+      self.time_period = time_period
+      self.target_audience = target_audience
+      self.ending = ending
+      self. creations = [] #list to stor the created fanart or fanfiction
+
+    def read(self):
+      print("\n--- Reading Book Summary ---")
+      print(f"Author: {self.author}")
+      print(f"Main Chracter: {self.main_character}")
+      print(f"Time Period: {self.time_period}")
+      print(f"Target Audience: {self.target_audience}")
+      print(f"Original Ending: {"self.ending}")
+
+    def reasearch(self):
+                                
+
+
+
+                                    
+                                
+                    
+  
