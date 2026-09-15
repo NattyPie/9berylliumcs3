fe1f1cd17e387120cf35d7f298e6fbfa708cc6d6
NattyPie <nppatole@brc.pshs.edu.ph>
2026-09-15 12:16:14 +0800

    Add section headers for class relationships documentation

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,6 +1,8 @@
 # Class Relationships: Association and Multiplicity
+
 ## Previous Work
 [Part I - Classes and Objects](classObjectUML.md)
+
 [Part II - Class Attributes and Methods](classAttributesMethods.md)
 ## Existing Class
 Class:
