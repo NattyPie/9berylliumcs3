e655ec465fcc0168a191cf1973b4fcd3f4371433
NattyPie <nppatole@brc.pshs.edu.ph>
2026-09-15 12:15:42 +0800

    Update classRelationships.py

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,1 +1,14 @@
-
+# Class Relationships: Association and Multiplicity
+## Previous Work
+[Part I - Classes and Objects](classObjectUML.md)
+[Part II - Class Attributes and Methods](classAttributesMethods.md)
+## Existing Class
+Class:
+Description:
+## New Related Class
+Class:
+Description:
+## Association
+Relationship:
+Explanation:
+## Multiplicity
